55f82df4ecabe64a5ecbc0de072c40d7fd9d2d4c
Thomas Greenman <thomasg@aja.com>
2024-06-07 17:39:54 -0700

    MBedTLS #1130 Check if _WIN32, not WINDOWS

--- include/mbedtls/export.h
+++ include/mbedtls/export.h
@@ -1,7 +1,7 @@
 #ifndef EXPORT_H
 #define EXPORT_H
 
-#if defined(WINDOWS) && defined(BUILDING_SHARED)
+#if defined(_WIN32) && defined(BUILDING_SHARED)
 	#define EXPORT __declspec(dllexport)
 #else
 	#define EXPORT extern
